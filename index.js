3fa8fb2e0d436414cf75150b2ca4a18e6ce8c2d7
Eslam Wael <103150616+Eslam10497@users.noreply.github.com>
2022-06-08 02:11:28 +0200

    Refactor
    Removing unwanted commands

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-randomNumber1 = Math.floor((Math.random()*6)+1);
-randomNumber2 = Math.floor((Math.random()*6)+1);
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+var randomNumber2 = Math.floor((Math.random()*6)+1);
 var player1 = "images/dice"+randomNumber1+".png";
 var player2 = "images/dice"+randomNumber2+".png";
 document.querySelector(".img1").setAttribute("src", player1);
@@ -15,3 +13,4 @@ if(randomNumber1 < randomNumber2){
 if(randomNumber1 === randomNumber2){
   document.querySelector("h1").textContent = "It's a Draw !!";
 }
+
